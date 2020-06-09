83518e522724dfd7d3b8edee90c1205b2c787802
Mihai-Flavius Iftode <MHAI@simcorp.com>
2020-06-09 18:35:07 +0200

    Remove .gitignore screw-ups

--- Exchange/Implementation/ExchangeRateHolder.cs
+++ Exchange/Implementation/ExchangeRateHolder.cs
@@ -1,19 +1,30 @@
 using System.Collections.Generic;
+using Exchange.Contracts;
 
-namespace Exchange
+namespace Exchange.Implementation
 {
-    public class ExchangeRateHolder
+    public class ExchangeRateHolder : IExchangeRateHolder
     {
-        public readonly Dictionary<string, double> ExchangeRates = new Dictionary<string, double>
+        public double BaseAmount { get; }
+
+        public Dictionary<string, double> ExchangeRates { get; }
+
+        public ExchangeRateHolder(double baseAmount)
         {
-            {"EUR", 743.94},
-            {"USD", 663.11},
-            {"GBP", 852.85},
-            {"SEK", 76.10},
-            {"NOK", 78.40},
-            {"CHF", 683.58},
-            {"JPY", 7.9748},
-            {"DKK", 100.00},
-        };
+            BaseAmount = baseAmount;
+
+            ExchangeRates = new Dictionary<string, double>
+            {
+                {"EUR", 743.94},
+                {"USD", 663.11},
+                {"GBP", 852.85},
+                {"SEK", 76.10},
+                {"NOK", 78.40},
+                {"CHF", 683.58},
+                {"JPY", 7.9748},
+                // Cache the base amount for easy access
+                {"DKK", BaseAmount},
+            };
+        }
     }
 }
